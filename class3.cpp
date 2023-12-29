cbf5777303e34909b6c75b2e65f82d8428ee2b5a
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-29 23:02:24 +0545

    to access the object assigned value

--- class3.cpp
+++ class3.cpp
@@ -11,11 +11,27 @@ public:
         return num1 + num2;
     }
 };
+class N1{
+    public:
+    int n1;
 
+};
+class N2{
+    public:
+    int n2;
+
+};
 int main() {
     Add a;
     a.num1 = 64;
     a.num2 = 42;
     cout << "The sum of two numbers is: " << a.add() << endl;
+
+    N1 Num1;
+    Num1.n1=64;
+    N2 Num2;
+    Num2.n2=42;
+    cout << "The sum of two numbers is: " << Num1.n1+ Num2.n2<< endl;
+
     return 0;
 }
