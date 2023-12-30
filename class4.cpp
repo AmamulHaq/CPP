d8ba0bd7d9eab788eddcb80005cc41e102ba8290
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-30 23:02:54 +0545

    simple class 4

---
+++ class4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class student
+{
+public:
+    char name[20];
+    int rollno;
+    float marks;
+};
+int main()
+{
+    student s1;
+    cout << "Enter name:" << endl;
+    cin >> s1.name;
+    cout << "Enter roll no:" << endl;
+    cin >> s1.rollno;
+    cout << "Enter marks:" << endl;
+    cin >> s1.marks;
+    cout << "Name:" << s1.name << "\t Rollno:" << s1.rollno << "\t" << s1.marks << endl;
+}
