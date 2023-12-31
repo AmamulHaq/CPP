a54d6a30fe822cf2df9730c7a3b61cbc377ba074
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-31 13:34:53 +0545

    area of circle

---
+++ practical1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+const float PI = 3.1415;
+float area,r;
+cout<<"Enter radius of a circle:";
+cin>>r;
+area=PI*r*r;
+cout<<"The area of circle is: "<<area<<endl;
+
+
+}
