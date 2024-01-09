f79b2ba4696a5d1f7cdea4337907f2fecb7f1d9f
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-01-09 22:39:10 +0545

    dynamic memory allocation

--- p8.cpp
+++ p8.cpp
@@ -3,3 +3,24 @@
 #include <cmath>
 
 using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter total number of students: ";
+    cin >> num;
+    float *ptr;
+    ptr = new float[num];
+    cout << "Enter GPA of students." << endl;
+    for (int i = 0; i < num; ++i)
+    {
+        cout << "student" << i + 1;
+        cin >> *(ptr + i);
+    }
+    cout << "\nDisplaying GPA of students." << endl;
+    for (int i = 0; i < num; ++i)
+    {
+        cout << "student" << i + 1 << ": " << *(ptr + i) << endl;
+    }
+    delete[] ptr;
+    return 0;
+}
