14d8b38be3d306e735b690ec37e7c62f80db1e4f
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-01-20 22:19:52 +0545

    imlementation of pure virtual function

--- p15.cpp
+++ p15.cpp
@@ -1,6 +1,27 @@
 
 #include <iostream>
 #include <string>
-
 using namespace std;
-// just to make contribution
+class shape{
+    public:
+   virtual void draw()=0;
+};
+class circle: public shape{
+public:
+void draw(){cout<<"\nDerived1: drawing a circle...";}
+};
+class rectangle: public shape{
+public:
+void draw(){cout<<"\nDerived2: drawing a rectangle...";}
+};
+int main(){
+    shape*ptr[2];
+    circle c;
+    rectangle r;
+    ptr[0]=&c;
+    ptr[1]=&r;
+    ptr[0]->draw();
+    ptr[1]->draw();
+    return 0;
+}
+
