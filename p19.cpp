3e88bb003f1f79c544101654dd71596a8535571e
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-01-22 21:29:12 +0545

    to check terminal

---
+++ p19.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class box
+{
+    float l, b, h;
+
+public:
+    // Constructor
+    box(float le, float br, float he)
+    {
+        l = le;
+        b = br;
+        h = he;
+    }
+
+    // Member function to calculate and display the volume
+    void volume()
+    {
+        float vol = l * b * h;
+        cout << "The volume of the box is: " << vol << endl;
+    }
+};
+
+int main()
+{
+
+    box myBox(10, 5.5, 3.5);
+
+    // Calling the volume method on the created object
+    myBox.volume();
+
+    return 0;
+}
