def1fbb0a49f2d4f1a4a2bf6493580fb69051d1f
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-02-10 23:17:14 +0545

    output function

--- p23.cpp
+++ p23.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-#include <string>
+#include <cstring> // Include the header for strlen function
 
 using namespace std;
 
 int main()
-{}
+{
+    int size = 20, i, m, n;
+    char name[24];
+    char str1[] = "oop ", str2[] = "notebook";
+    cout << "\nEnter name: ";
+    cin.getline(name, size);
+    cout << "Your name is: " << name << endl; // Added a space after "Your name is"
+    m = strlen(str1); // Corrected the function name to strlen
+    n = strlen(str2);
+    for (i = 0; i < n; i++)
+    {
+        cout.write(str2, 1);
+        cout<<endl;
+    }
+    for (i = n; i > 0; i--)
+    {
+        cout.write(str2, i);
+        cout<<endl;
+    }
+    return 0;
+}
