86904ff542538d56b31ad58c99f520bccb2ad641
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-02-18 14:38:35 +0545

    BEI students

---
+++ school.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Class
+{
+private:
+    char name[24];
+    int roll;
+    int marks[3];
+
+public:
+    void input()
+    {
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter roll: ";
+        cin >> roll;
+        cout << "Enter marks: ";
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> marks[i];
+        }
+    }
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Roll: " << roll << endl;
+        cout << "Marks: ";
+        for (int i = 0; i < 3; i++)
+        {
+            cout << marks[i] << "\t";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    Class BEI[3];
+    cout << "Enter details of BEI students\n";
+    for (int i = 0; i < 3; i++)
+    {
+        BEI[i].input();
+    }
+    cout << "\nStudent details:\n";
+    for (int i = 0; i < 3; i++)
+    {
+        BEI[i].display();
+    }
+
+    return 0;
+}
