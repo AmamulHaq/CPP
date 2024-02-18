763afcb686bcdb5a6d0fd9a313270cfff1336619
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-02-18 08:09:42 +0545

    dynamic memory allocation

--- p28.cpp
+++ p28.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    int *ptr;
+    ptr = new int;
+    *ptr = 786;
+    cout << "the number is : " << *ptr<<endl;
+    delete ptr;
+
+    *ptr = 78690;
+
+    cout << "the number after delete is: " << *ptr;
+
+    return 0;
+}
