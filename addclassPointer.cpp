5b69c332ac92dda5faee5b77b7008c47094571a0
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-03-11 21:48:22 +0545

    add classes

---
+++ addclassPointer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Sum
+{
+private:
+    float s;
+public:
+    float add(float *a, float *b)
+    {
+        s = *a + *b;
+        return s;
+    }
+};
+
+int main()
+{
+    float a = 4.3, b = 6.5;
+    Sum s;
+    cout << "The sum of two data members is: " << s.add(&a, &b) << endl;
+}
