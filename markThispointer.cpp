ad0c9f155926a8ba46378e8ad8a660e2782df75d
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-03-12 10:02:18 +0545

    this pointer

---
+++ markThispointer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class sub
+{
+private:
+    float mark;
+
+public:
+//can access data members and memory address of an object
+    void getmark(float mark)
+    {
+        this->mark = mark;
+    }
+    void display()
+    {
+        cout << "the mark is: " << mark << endl;
+        cout << "the address is: " << this<<endl;
+    }
+};
+int main()
+{
+    sub math, phy;
+    math.getmark(92);
+    math.display();
+    phy.getmark(86);
+    phy.display();
+
+    return 0;
+}
+
